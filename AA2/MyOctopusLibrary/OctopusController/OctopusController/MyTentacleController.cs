c9a4815322296d2b619bf43415f9cb484965d822
llucferrando <lluc.ferrando@enti.cat>
2023-11-02 09:43:37 +0100

    Folders requierments

---
+++ AA2/MyOctopusLibrary/OctopusController/OctopusController/MyTentacleController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+
+
+
+namespace OctopusController
+{
+
+    
+    internal class MyTentacleController
+
+    //MAINTAIN THIS CLASS AS INTERNAL
+    {
+
+        TentacleMode tentacleMode;
+        Transform[] _bones;
+        Transform _endEffectorSphere;
+
+        public Transform[] Bones { get => _bones; }
+
+        //Exercise 1.
+        public Transform[] LoadTentacleJoints(Transform root, TentacleMode mode)
+        {
+            //TODO: add here whatever is needed to find the bones forming the tentacle for all modes
+            //you may want to use a list, and then convert it to an array and save it into _bones
+            tentacleMode = mode;
+
+            switch (tentacleMode){
+                case TentacleMode.LEG:
+                    //TODO: in _endEffectorsphere you keep a reference to the base of the leg
+                    break;
+                case TentacleMode.TAIL:
+                    //TODO: in _endEffectorsphere you keep a reference to the red sphere 
+                    break;
+                case TentacleMode.TENTACLE:
+                    //TODO: in _endEffectorphere you  keep a reference to the sphere with a collider attached to the endEffector
+                    break;
+            }
+            return Bones;
+        }
+    }
+}
